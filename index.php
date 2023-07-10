cc2893987b21c38a66fd5a8d87351f366e1f941e
inchan Tak <ict970@gmail.com>
2023-07-10 14:21:32 +0900

    로그인 및 회원가입  추가중

--- index.php
+++ index.php
@@ -23,23 +23,39 @@
     <section class="two">
 	
     <div id="count">
-		<!-- 하루 걸음 수가 0 일경우 걸음 수를 입력하는 창을 보이게 한다. -->
-		<?php if($today !=0) 
-		{echo "
-	<div id='count_title'>오늘의 걸음 수</div>
-	<div id='num'> $today 걸음</div>
-	<progress id='progress' value='$today' min='0' max='10000'></progress>
-	<!-- 오늘 걸음 수를 가져와서 해당 값을 프로세스바를 통해서 보여준다. -->
+		
+		<?php 
+		if(isset($_SESSION['userId'])) {
+			// 세션에 아이디 값이 없으면 로그인 화면을 출력하게 한다
+		if($today !=0) 
+		    //하루 걸음 수가 0 일경우 걸음 수를 입력하는 창을 보이게 한다.
+		{echo 
+
+		"<div id='count_title'>오늘의 걸음 수</div>
+		<div id='num'> $today 걸음</div> 
+		<progress id='progress' value='$today' min='0' max='10000'></progress>
+		<!-- 오늘 걸음 수를 가져와서 해당 값을 프로세스바를 통해서 보여준다. -->
 		
 		";}else{
 		echo 
+
 		"<div id='count_title'>오늘 하루 얼마나 걸으셨나요?</div>
 		<form method='POST' action='db/walk_insert.php'>
 		<input type='number' id='walk_input' placeholder='하루동안 걸은 걸음 수' name='today_walk'>
 		<input type='submit' id='walk_submit' value='입력'>
 		</form>
 		
-		";}?>
+		";}
+		}else{
+			echo
+			"
+			<div id='count_title'>하루 걸음을 기록해 보세요</div>
+			<form method='POST' action='db/walk_login.php'>
+			<input type='submit' id='login' value='로그인' >
+			</form>
+			<a href='db/walk_join.php' id='join'>계정이 없으신가요?</a>
+			";
+		}?>
 
 	</div>
 
